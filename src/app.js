c8d019c7e3556b86cbb9632aa9cf4fa9479ffdb1
andresjaromezuk <andres.jaromezuk@gmail.com>
2022-12-08 12:44:13 -0300

    cambios app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 import express from 'express';
 import {PORT} from './db/config.js'
+import {pool} from './db/db.js'
 
 const app = express();
 
 
-app.get('/', (req, res)=>{
-    res.send('Hola Mundo!')
+app.get('/', async (req, res)=>{
+    let [user] = await pool.query('SELECT * FROM users')
+    res.json(user[0]);
 })
+
+app.get('/create', (req, res)=>{
+    pool.query('INSERT INTO users(name) VALUES ("John")')
+})
+
+
 
 app.listen(PORT)
 console.log('servidor funcionando en el puerto 3000')
